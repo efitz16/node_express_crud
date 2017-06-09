9f60ef29eb7de0fd9bfc3657a517da86b6332dd1
Elizabeth Fitzpatrick <elizabethfitzpatrick16@gmail.com>
2017-06-08 23:27:01 -0400

    before I mess with speakers route

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(request, response) {
+  var data = req.app.get('writersData');
+  // var writersPhotos = [];
+
+  // data.writers.forEach(function(item){
+  // 	writersPhotos = writersPhotos.concat(item.photo);
+  // })
+
   response.render('index', {
   	pageTitle: 'Writers Home',
   	pageId: 'home'
+  	// photo: writersPhotos
   });
 });
 
